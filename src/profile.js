f17f2ff34590f3bcaaeee39d4e6304b780c1a76e
Julian Quino <julian.quino2@gmail.com>
2023-09-25 00:21:10 -0300

    okteto configuration

--- src/profile.js
+++ src/profile.js
@@ -4,21 +4,24 @@ const verifyToken = require('./tokenMiddleware');
 
 const router = Router();
 
-router.use('/', async (req, res) => {
+router.use('/', (req, res) => {
     try{
-        const username = verifyToken(req.headers.token);
-
-        const response = await axios({
-            method: req.method,
-            url: process.env.PROFILE_URL + req.path,
-            data: req.method != 'GET' ? {...req.body, username: username} : {},
-            params: req.query,
-        });
-
-        res.status(response.status).json(response.data);
+        var username = verifyToken(req.headers.token);
     } catch(err){
         res.status(err.statusCode ?? 500).json({ message: err.message ?? 'An unexpected error has occurred. Please try again later.' });
+        return ;
     }
+    axios({
+        method: req.method,
+        url: process.env.PROFILE_URL + req.path,
+        data: req.method != 'GET' ? {...req.body, username: username} : {},
+        params: req.query,
+    }).then((response) => {
+        res.status(response.status).json(response.data);
+    }).catch((err)=>{
+        console.log(err);
+        res.status(err.response.status).json(err.response.data);
+    });
 });
 
 module.exports = router;
